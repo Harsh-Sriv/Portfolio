ac5981c519efa4e29ee9c8785e2fa8f67ec0b584
Harsh Srivastava <138398088+Harsh-Sriv@users.noreply.github.com>
2025-07-31 01:47:48 +0530

    Making a Portfolio Website

--- Portfolio-Harsh Srivastava/src/App.jsx
+++ Portfolio-Harsh Srivastava/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './CSS/App.css'
-import Navbar from './Components/navbar.jsx'
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/navbar';
+import Home from './pages/Home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Projects from './pages/projects';
 
 function App() {
-  return(
-    <>
-    <Navbar />
-    <h1>Harsh Srivastava</h1>
-    </>
-  )
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- Portfolio-Harsh Srivastava/src/Components/navbar.jsx
+++ Portfolio-Harsh Srivastava/src/Components/navbar.jsx
@@ -1,16 +1,19 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/Name White.png';
 import darkMode from '../assets/darkmode.svg';
 import '../CSS/navbar.css';
 
 function Navbar() {
   return (
-    <header class='navbar'>
-      <img src={logo} alt="My logo" class="logo react" />
-      <nav>
-        <p>Home</p>
-        <p>About</p>
-        <p>Projects</p>
-        <p>Contact</p>
+    <header className='navbar'>
+        <div className="image container">
+            <img src={logo} alt="My logo" className="logo react" />
+        </div>
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
 
       <img src={darkMode} alt="Dark Mode" />
